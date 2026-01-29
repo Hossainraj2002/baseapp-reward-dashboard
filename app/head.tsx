7078146fbc45f3d6a5e5bdace222bbad08dfa576
Hossain Raj <76206951+Hossainraj2002@users.noreply.github.com>
2026-01-29 17:37:48 +0600

    Add Head component for metadata and title

---
+++ app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="base:app_id" content="6970f9825f24b57cc50d3331" />
+      <title>Baseapp Reward Dashboard</title>
+    </>
+  );
+}
